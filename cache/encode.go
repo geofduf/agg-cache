03c7da59857c147234720fecdaebe575f53471ee
Geof <46729592+geofduf@users.noreply.github.com>
2023-04-02 19:11:54 +0200

    fix delta encoding

--- cache/encode.go
+++ cache/encode.go
@@ -5,18 +5,24 @@ import (
 )
 
 func encode(value *int64, previousValue *int64) (byte, int64) {
-	if x := *value; (x > 63 || x < -63) && previousValue != nil {
-		y := x - *previousValue
-		delta := y
-		if x < 0 {
-			x = -x
-		}
-		if y < 0 {
-			y = -y
-		}
-		if x > y && bits.Len64(uint64(x))/7 > bits.Len64(uint64(y))/7 {
+	x := *value
+	if (x > 63 || x < -64) && previousValue != nil {
+		delta := x - *previousValue
+		// TODO: discard obvious cases
+		if varintSize(delta) < varintSize(x) {
 			return encodedFlag, delta
 		}
 	}
-	return regularFlag, *value
+	return regularFlag, x
+}
+
+func varintSize(x int64) int {
+	if x < 0 {
+		x = -x - 1
+	}
+	n := bits.Len64(uint64(x)) + 1
+	if n%7 != 0 {
+		return n/7 + 1
+	}
+	return n / 7
 }
